c27ba38cbfaff371f0f3b4563217a628e9ff3fd0
Sunny-Wolfy <aleksandrova-valeria-01@yandex.ru>
2024-09-10 15:15:57 +0300

    github btn fix

--- my-app/src/components/btnGitHub/BtnGitHub.jsx
+++ my-app/src/components/btnGitHub/BtnGitHub.jsx
@@ -3,7 +3,7 @@ import "./styles.css";
 export default function BtnGitHub({ link }) {
   return (
     <a href={link} target="_blank" rel="noreferrer" className="btn-outline">
-      <img src="./img/icons/gitHub-black.svg" alt="" />
+      <img src="/img/icons/gitHub-black.svg" alt="" />
       GitHub repo
     </a>
   );
