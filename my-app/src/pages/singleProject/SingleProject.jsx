cc3c3ff460795590dd877c3d96e108b741066f29
Sunny-Wolfy <aleksandrova-valeria-01@yandex.ru>
2024-09-10 15:15:50 +0300

    create render single project

--- my-app/src/pages/singleProject/SingleProject.jsx
+++ my-app/src/pages/singleProject/SingleProject.jsx
@@ -1,24 +1,26 @@
 import { useParams } from "react-router-dom";
 import BtnGitHub from "../../components/btnGitHub/BtnGitHub";
+import { projectList } from "../../helpers/projectList";
 
 import "./styles.css";
 
 export default function SingleProject() {
-  const {id} = useParams();
+  const { id } = useParams();
+  const data = projectList[id];
   return (
     <main className="section">
       <div className="container">
         <div className="project-details">
-          <h1 className="title-1">Video service</h1>
+          <h1 className="title-1">{data.title}</h1>
 
           <img
-            src="/img/projects/02-big.jpg"
-            alt=""
+            src={`/img/projects/${data.imgBig}`}
+            alt={data.title}
             className="project-details__cover"
           />
 
           <div className="project-details__desc">
-            <p>Skills: React, Node.js, MongoDB</p>
+            <p>Skills: {data.skills}</p>
           </div>
 
           <BtnGitHub link="https://github.com" />
