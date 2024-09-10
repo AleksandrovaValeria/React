1a050a3d5eab49619f170a6b39710477c7b2e131
Sunny-Wolfy <aleksandrova-valeria-01@yandex.ru>
2024-09-10 17:29:47 +0300

    remove useRef

--- my-app/src/components/btnTheme/BtnTheme.jsx
+++ my-app/src/components/btnTheme/BtnTheme.jsx
@@ -1,19 +1,16 @@
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import { useLocalStorage } from "../../utils/useLocalStorage";
 import detectDarkMode from "../../utils/detectDarkMode";
 import "./styles.css";
 
 export default function BtnTheme() {
   const [theme, setTheme] = useLocalStorage("theme", detectDarkMode());
-  const btnRef = useRef(null);
 
   useEffect(() => {
     if (theme === "dark") {
       document.body.classList.add("dark");
-      btnRef.current.classList.add("dark-mode-btn--active");
     } else {
       document.body.classList.remove("dark");
-      btnRef.current.classList.remove("dark-mode-btn--active");
     }
   }, [theme]);
 
@@ -24,14 +21,21 @@ export default function BtnTheme() {
         const newTheme = event.matches ? "dark" : "light";
         setTheme(newTheme);
       });
-  }, []);
+  }, [setTheme]);
 
   function changeTheme() {
     setTheme((currentValue) => (currentValue === "light" ? "dark" : "light"));
   }
 
   return (
-    <button ref={btnRef} className="dark-mode-btn" onClick={changeTheme}>
+    <button
+      className={
+        theme === "dark"
+          ? "dark-mode-btn dark-mode-btn--active"
+          : "dark-mode-btn"
+      }
+      onClick={changeTheme}
+    >
       <img
         src="/img/icons/sun.svg"
         alt="Light mode"
